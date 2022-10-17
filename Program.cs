cf150c25b8fd1643e53add4ec74cf8912e30232f
Anirudha Gupta <anirudhagupta01@gmail.com>
2022-10-17 14:13:01 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,23 +8,37 @@ namespace TwoSumCsharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            TwoSum(new int[] { 2, 7, 11, 15 }, 9);
-            //TwoSum(new int[] { 3, 2, 4 }, 6);
+            //TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            TwoSum(new int[] { 3, 2, 4 }, 6);
         }
 
-        public static int[] TwoSum(int[] nums, int target)
+        public static int[] TwoSum(IEnumerable<int> nums, int target)
         {
-            int[] result = new int[2];
-            for (int i = 0; i < nums.Length; i++)
+            int index = 0;
+            int count = nums.Count();
+
+            foreach (int x in nums)
             {
-                int index = Array.FindIndex(nums.Skip(i + 1).ToArray(), x => x == target - nums[i]);
-                if (index != -1)
-                {
-                    result = new int[] { i, index + (i+1) };
-                    break;
-                }
+                int remain = target - x;
+                int secondIndex = FindIndex(nums.Skip(index + 1), remain);
+                if (secondIndex != -1)
+                    return new int[] { index, secondIndex + index + 1 };
+                index++;
             }
-            return result;
+            return new int[] { };
+        }
+
+        private static int FindIndex(IEnumerable<int> nums, int target)
+        {
+            int index = -1;
+            int count = nums.Count();
+            foreach (var num in nums)
+            {
+                index++;
+                if (num == target)
+                    return index;
+            }
+            return -1;
         }
     }
 
