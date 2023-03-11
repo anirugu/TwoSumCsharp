7629938c7e453cd933356c550699b17fa387c52f
Anirudha Gupta <anirudhagupta01@gmail.com>
2023-03-11 09:25:17 +0530

    Reduce the need fo call .Count multiple time.
    Write a method that search in specific range without using Linq. Current ET = 151MS https://leetcode.com/problems/two-sum/submissions/913000008/

--- Program.cs
+++ Program.cs
@@ -8,37 +8,10 @@ namespace TwoSumCsharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Solution solution = new Solution();
+
             //TwoSum(new int[] { 2, 7, 11, 15 }, 9);
-            TwoSum(new int[] { 3, 2, 4 }, 6);
-        }
-
-        public static int[] TwoSum(IEnumerable<int> nums, int target)
-        {
-            int index = 0;
-            int count = nums.Count();
-
-            foreach (int x in nums)
-            {
-                int remain = target - x;
-                int secondIndex = FindIndex(nums.Skip(index + 1), remain);
-                if (secondIndex != -1)
-                    return new int[] { index, secondIndex + index + 1 };
-                index++;
-            }
-            return new int[] { };
-        }
-
-        private static int FindIndex(IEnumerable<int> nums, int target)
-        {
-            int index = -1;
-            int count = nums.Count();
-            foreach (var num in nums)
-            {
-                index++;
-                if (num == target)
-                    return index;
-            }
-            return -1;
+            solution.TwoSum(new int[] { 3, 2, 4 }, 6);
         }
     }
 
---
+++ Solution.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var remain = target - nums[i];
+            var index = FindIndex(remain, i + 1, nums);
+            if (index != -1)
+                return new int[] { i, index };
+        }
+        return new int[] { };
+    }
+
+    private int FindIndex(int item, int startIndex, int[] items)
+    {
+        for (int i = startIndex; i < items.Length; i++)
+        {
+            if (items[i] == item)
+                return i;
+        }
+        return -1;
+    }
+}
